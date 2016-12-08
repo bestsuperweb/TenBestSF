803e87df91be1a0a07cf17b42bb51fca3eccf9da
Tom <bestsuperweb@gmail.com>
2016-12-08 06:47:44 -0500

    add test for index controller...

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'jbuilder', '~> 2.5'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'sqlite3'
   gem 'byebug', platform: :mri
 end
 
@@ -39,9 +40,14 @@ group :development do
   gem 'listen', '~> 3.0.5'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
-  # Use sqlite3 as the database for Active Record
-  gem 'sqlite3'
+  gem 'spring-watcher-listen', '~> 2.0.0'  
+end
+
+group :test do
+  gem 'rails-controller-testing', '0.1.1'
+  gem 'minitest-reporters',       '1.1.9'
+  gem 'guard',                    '2.13.0'
+  gem 'guard-minitest',           '2.4.4'
 end
 
 group :production do
--- test/controllers/index_controller_test.rb
+++ test/controllers/index_controller_test.rb
@@ -4,4 +4,32 @@ class IndexControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @base_title = "10 Best SF"
+  end
+
+  test "should get home" do
+    get home_url
+    assert_response :success
+    assert_select 'title', "Best of SF City Guide | Top Restaurants Bars & Events | #{@base_title}"
+  end
+
+  test "should get about" do
+    get about_url
+    assert_response :success
+    assert_select 'title', "About Us | What We Do | #{@base_title}"
+  end
+
+  test "should get place" do
+    get place_url
+    assert_response :success
+    assert_select 'title', "PLACE NAME | Top Brunch in SF | #{@base_title}"
+  end
+
+  test "should get ranking" do
+    get ranking_url
+    assert_response :success
+    assert_select 'title', "Best Brunch in SF | Top Brunch Restaurant SF | #{@base_title}"
+  end
+
 end
